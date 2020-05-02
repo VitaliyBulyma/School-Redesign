38cfa083f80430ef7fcf1619b7d6f7bb377f5125
VitaliyBulyma <vitaliy_bulyma@yahoo.ca>
2020-05-02 18:44:48 -0400

    about-us page added

--- functions.php
+++ functions.php
@@ -6,8 +6,13 @@ function school_files(){
     wp_enqueue_script('main-school-js', get_theme_file_uri('js/scripts-bundled.js'), NULL, microtime(), true); //use '0.0' instead of microtime() on live site
     wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('school_main_styles', get_stylesheet_uri());
+    wp_enqueue_style('school_main_styles', get_stylesheet_uri(), NULL, microtime()); //add NULL, microtime() to remove caching by browser
 }
 add_action('wp_enqueue_scripts', 'school_files');
+
+function school_features(){
+    add_theme_support('title-tag'); //add title to the page
+}
+add_action('after_setup_theme', 'school_features');
 
 ?>
