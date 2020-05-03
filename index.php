ef6cc8708ba9beaabb3b4ae996a6fb775e45eec0
VitaliyBulyma <vitaliy_bulyma@yahoo.ca>
2020-05-03 12:51:30 -0400

    nav for children pages added

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php get_header(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg'); ?>"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/school.jpg'); ?>"></div>
     <div class="page-banner__content container t-center c-white">
-      <h1 class="headline headline--large">Welcome!</h1>
-      <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-      <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-      <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+      <h1 class="headline headline--large">Хмельницька Гімназія №1</h1>
+      <h2 class="headline headline--medium">імені Володимира Красицького</h2>
+      <h3 class="headline headline--small">Умови вступу</h3>
+      <a href="https://drive.google.com/file/d/1r6vPJwhbK77tOgxaJkiQNLXU0vaVfBNm/view" target="_blank" class="btn btn--large btn--blue">Переглянути</a>
     </div>
   </div>
+
 
   <div class="full-width-split group">
     <div class="full-width-split__one">
